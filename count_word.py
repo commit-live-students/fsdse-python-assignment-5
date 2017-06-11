f9fe90d12732d70ca6c593ef4865b60800dba61a
coded9 <coded9@users.noreply.github.com>
2017-06-11 06:58:41 +0000

    Done

--- count_word.py
+++ count_word.py
@@ -1,13 +1,16 @@
 
 def countWord(filepath,testWord):
-    fp = open(filepath,"r")
+    #fp = open(filepath,"r")
     wordDict = {}
-    for line in fp.readlines():
-        print line
-        for word in line.split(" "):
-            if word in wordDict:
-                wordDict[word] += 1
-            else:
-                wordDict[word] = 1
+    #for line in fp.readlines():
+    with open(filepath,"r") as file:
+        for line in file:
+            print line
+            for word in line.split(" "):
+                if word in wordDict:
+                    wordDict[word] += 1
+                else:
+                    wordDict[word] = 1
+            #print wordDict[testWord]
     return wordDict[testWord]
-#countWord("files/testfile.txt","Lorem")
+countWord("files/testfile.txt","Lorem")
